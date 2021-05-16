c313d5254cf828a6befb15e7e5b9860b9785fe8c
Smolyaninala <Smolyaninala@arkona36.ru>
2021-05-16 13:09:47 +0300

    Добавьте файлы проекта.

---
+++ BlockScheme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace BlockScheme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { IsPremium = false, Login = "Petrov", Name = "Иван" }) ;
+            users.Add(new User() { IsPremium = true, Login = "Sidorov", Name = "Алексей" });
+            users.Add(new User() { IsPremium = true, Login = "Ivanov", Name = "Андрей" });
+
+
+            foreach (User user in users)
+            {
+                Console.WriteLine("Здравствуйте {0}", user.Name);
+                if(!user.IsPremium)
+                {
+                    User.ShowAds();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BlockScheme/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace BlockScheme
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+
+		public static void ShowAds()
+		{
+			Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+			
+			Thread.Sleep(1000);
+
+			Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+			
+			Thread.Sleep(1000);
+
+			Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+			
+			Thread.Sleep(1000);
+		}
+	}
+}
